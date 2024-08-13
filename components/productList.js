83fff5c4c95a75ceb19ad4ec9b6d4a41e7f14b13
Guy D <guydaane@gmail.com>
2024-08-13 03:12:50 +0300

    grid design

--- components/productList.js
+++ components/productList.js
@@ -1,10 +1,11 @@
 import ProductCard from "./productCard";
+//eslind-disable-file
 
 function ProductList({ products }) {
     return (
       <div className="container mx-auto px-4 flex flex-wrap justify-center">
         {products.map((product) => (
-          <div className="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 2xl:w-1/3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl max-h-full p-4">
+          <div key={list} className="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 2xl:w-1/3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl max-h-full p-4">
             <ProductCard key={product.id} product={product} />
           </div>
         ))}
