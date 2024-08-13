d283ba1e1165e413eaec1520c3a843cdbf96f553
Guy D <guydaane@gmail.com>
2024-08-13 03:08:05 +0300

    grid design

--- components/productList.js
+++ components/productList.js
@@ -4,7 +4,7 @@ function ProductList({ products }) {
     return (
       <div className="container mx-auto px-4 flex flex-wrap justify-center">
         {products.map((product) => (
-          <div className="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl max-h-full p-4">
+          <div className="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 2xl:w-1/3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl max-h-full p-4">
             <ProductCard key={product.id} product={product} />
           </div>
         ))}
