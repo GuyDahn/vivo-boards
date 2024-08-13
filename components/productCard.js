6adef07acd1d85a0c69a35a9b954280c1f4ff824
Guy D <guydaane@gmail.com>
2024-08-13 03:33:07 +0300

    added data for assignement

--- components/productCard.js
+++ components/productCard.js
@@ -1,12 +1,16 @@
 function ProductCard({ product }) {
-        return (
-            <div className="border-2 border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-[344px] w-[344px]">
-                {product.images.length > 0 && (
-                    <img src={product.images[0].src} alt={product.title} className="w-full h-64 object-cover object-center mb-4 rounded" />
-                )}
-                <h2 className="text-1xl font-semibold mb-2 text-gray-800">{product.title}</h2>
-            </div>
-        );
+    console.log(product); // Add this line to log the product object
+
+    return (
+        <div className="border-2 border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-[400px] w-[320px]">
+            {product.images.length > 0 && (
+                <img src={product.images[0].src} alt={product.title} className="w-full h-64 object-cover object-center mb-4 rounded" />
+            )}
+            <h2 className="text-1xl font-semibold mb-2 text-gray-800">{product.title}</h2>
+            <div className="text-gray-600" dangerouslySetInnerHTML={{ __html: product.body_html }} />
+            <p className="text-gray-800 font-bold">{product.variants[0].price}</p>
+        </div>
+    );
 }
 
 export default ProductCard;
