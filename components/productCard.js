40291bd1b4388173161b561d72312af61a2eb136
Guy D <guydaane@gmail.com>
2024-08-13 11:56:25 +0300

    project wrapup

--- components/productCard.js
+++ components/productCard.js
@@ -1,23 +1,42 @@
-function ProductCard({ product }) {
-    console.log(product); // to see product data
+import { useState } from 'react'
 
-    return (
-        <div 
-            className="border-2 border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-[400px] w-[320px] flex flex-col justify-between bg-cover bg-center ml-2 my-8" 
-            style={{ backgroundImage: `url(${product.images.length > 0 ? product.images[0].src : ''})` }}
-        >
-            <div className="bg-white bg-opacity-50 p-4 rounded w-full">
-                <h2 className="text-1xl font-semibold mb-2 text-gray-800">{product.title}</h2>
-                <div className="text-gray-600 text-xs" dangerouslySetInnerHTML={{ __html: product.body_html }} />
-            </div>
-            <div className="flex justify-between items-center mt-4 px-2">
-                <p className="text-gray-800 font-bold">${product.variants[0].price}</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Buy Now
-                </button>
-            </div>
-        </div>
-    );
+function ProductCard ({ product }) {
+  const [hover, setHover] = useState(false)
+
+  const image =
+    product.images.length > 0
+      ? hover && product.images.length > 1
+        ? product.images[1].src
+        : product.images[0].src
+      : null
+
+  return (
+    <div
+        className='border-2 border-gray-200 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 h-[410px] w-[340px] flex flex-col justify-between bg-cover bg-center ml-2 my-4'
+        style={image ? { backgroundImage: `url(${image})` } : {}}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+    >
+      <div className='bg-white bg-opacity-50 p-4 rounded w-full'>
+        <h2 className='text-1xl font-semibold mb-2 text-gray-800'>
+          {product.title}
+        </h2>
+        <div
+          className='text-gray-600 text-xs'
+          dangerouslySetInnerHTML={{ __html: product.body_html }}
+        />
+      </div>
+      <div className='flex justify-between items-center mt-4 px-2'>
+        <p className='text-gray-800 font-bold text-2xl bg-yellow-200 bg-opacity-50 px-2 py-1 rounded'>
+            
+          ${product.variants[0].price}
+        </p>
+        <button className='bg-blue-100 hover:bg-blue-500 text-gray-700 font-bold py-2 px-4 rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+          Choose Options
+        </button>
+      </div>
+    </div>
+  )
 }
 
-export default ProductCard;
+export default ProductCard
--- components/userbtn.js
+++ components/userbtn.js
@@ -1,23 +1,23 @@
 import Link from 'next/link'
 
 const Userbtn = () => {
-    return(
-        <Link href='/account' class='text-gray-700 hover:text-gray-900'>
-        <svg
-          class='h-6 w-6'
-          fill='none'
-          stroke='currentColor'
-          stroke-width='2'
-          viewBox='0 0 24 24'
-        >
-          <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            d='M5 8a7 7 0 1114 0 7 7 0 01-14 0zm14 14a9 9 0 01-18 0 9 9 0 0118 0z'
-          ></path>
-        </svg>
-      </Link>
-    )
+  return (
+    <Link href='/account' class='text-gray-700 hover:text-gray-900'>
+      <svg
+        class='h-6 w-6'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        viewBox='0 0 24 24'
+      >
+        <path
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          d='M5 8a7 7 0 1114 0 7 7 0 01-14 0zm14 14a9 9 0 01-18 0 9 9 0 0118 0z'
+        ></path>
+      </svg>
+    </Link>
+  )
 }
 
 export default Userbtn
